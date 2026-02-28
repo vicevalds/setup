6f5b868f4b9ddbf3f900ef069bb0ab2c88062308
vicevalds <122176839+vicevalds@users.noreply.github.com>
2026-02-28 12:41:42 -0300

    .

--- apps.sh
+++ apps.sh
@@ -4,7 +4,7 @@ firefox https://obsidian.md/download
 firefox https://code.visualstudio.com/docs/?dv=linux64_deb
 firefox https://discord.com/api/download?platform=linux&format=deb
 firefox https://telegram.org/dl/desktop/linux
-
+curl -fsS https://dl.brave.com/install.sh | sh
 wget -qO- https://get.pnpm.io/install.sh | sh -
 . ~/.bashrc
 pnpm env use --global lts
